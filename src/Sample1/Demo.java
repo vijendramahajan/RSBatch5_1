82fcb2b7fe554885782f7a4274c782996675341a
Admin <Admin@WINDOWS-R8MVF0H>
2022-10-06 21:04:43 +0530

    gfgkugyl

--- src/Sample1/Demo.java
+++ src/Sample1/Demo.java
@@ -1,5 +1,12 @@
 package Sample1;
 
-public class Demo {
+public class Demo 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello");
+		System.out.println("Hi");
+		
+	}
 
 }
